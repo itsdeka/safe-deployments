4fe7e3414d8a0b43d391a6c16877fdfaf9f94c8d
Dario Moceri <dario.moceri@bitfinex.com>
2025-05-21 09:02:48 +0200

    export fixs

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,50 @@
-export * from './types';
-export * from './safes';
-export * from './factories';
-export * from './libs';
-export * from './handler';
-export * from './accessors';
+// Types
+export type { AddressType, SingletonDeploymentJSON, SingletonDeployment, SingletonDeploymentV2, DeploymentFilter } from './types';
+export { DeploymentFormats } from './types';
+
+// Safe deployments
+export { 
+  getSafeSingletonDeployment,
+  getSafeSingletonDeployments,
+  getSafeL2SingletonDeployment,
+  getSafeL2SingletonDeployments
+} from './safes';
+
+// Library deployments
+export {
+  getMultiSendDeployment,
+  getMultiSendDeployments,
+  getMultiSendCallOnlyDeployment,
+  getMultiSendCallOnlyDeployments,
+  getCreateCallDeployment,
+  getCreateCallDeployments,
+  getSignMessageLibDeployment,
+  getSignMessageLibDeployments,
+  getSafeMigrationDeployment,
+  getSafeMigrationDeployments,
+  getSafeToL2MigrationDeployment,
+  getSafeToL2MigrationDeployments,
+  getSafeToL2SetupDeployment,
+  getSafeToL2SetupDeployments
+} from './libs';
+
+// Factory deployments
+export {
+  getProxyFactoryDeployment,
+  getProxyFactoryDeployments
+} from './factories';
+
+// Handler deployments
+export {
+  getDefaultCallbackHandlerDeployment,
+  getDefaultCallbackHandlerDeployments,
+  getCompatibilityFallbackHandlerDeployment,
+  getCompatibilityFallbackHandlerDeployments,
+  getFallbackHandlerDeployment
+} from './handler';
+
+// Accessor deployments
+export {
+  getSimulateTxAccessorDeployment,
+  getSimulateTxAccessorDeployments
+} from './accessors';
